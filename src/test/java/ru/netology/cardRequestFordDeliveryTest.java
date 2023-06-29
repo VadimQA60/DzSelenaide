cc63bc4efea1229440cba03e698422408d2fcf4f
Vadim <vadim.penkin1997@mail.ru>
2023-06-29 10:31:02 +0300

    True commit

--- src/test/java/ru/netology/cardRequestFordDeliveryTest.java
+++ src/test/java/ru/netology/cardRequestFordDeliveryTest.java
@@ -1,22 +1,16 @@
 package ru.netology;
 
-import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class CardDeliveryTest {
+public class cardRequestFordDeliveryTest {
 
     private String getFutureDate(int addDays) {
         LocalDate currentDate = LocalDate.now();
@@ -38,10 +32,9 @@ public class CardDeliveryTest {
         $("[data-test-id=phone] input").setValue("+79168268994");
         $(".checkbox__box").click();
         $(".button").click();
-        WebElement title = $("[data-test-id=success-notification] .notification__title").shouldBe(visible, Duration.ofSeconds(15));
-        Assertions.assertEquals("Успешно!", title.getText());
-        WebElement content = $("[data-test-id=success-notification] .notification__content");
-        Assertions.assertEquals("Встреча успешно запланирована на " + getFutureDate(4), content.getText());
+        $("[data-test-id=success-notification] .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Успешно"));
+        $("[data-test-id=success-notification] .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Встреча успешно запланирована на " + getFutureDate(4)));
+
 
     }
 
